9bdab1b33697313a4fc7d446c42dc5351e50a20f
farhanariyanto03 <ariyantofarhan03@gmail.com>
2023-11-04 19:23:51 +0700

    komentar per baris

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,13 +1,16 @@
-import java.util.stream.DoubleStream;
+import java.util.stream.DoubleStream; // Kelas ini digunakan untuk melakukan operasi streaming pada bilangan pecahan ganda (double).
 
 public class calculator {
+    //Baris ini mendefinisikan metode statis bernama add dengan tipe pengembalian double.
     static double add(double... operands) {
+        //digunakan untuk mengubah larik (array) bilangan pecahan ganda (operands) menjadi
+        // sebuah aliran (stream) bilangan pecahan ganda.
         return DoubleStream.of(operands)
-                .sum();
+                .sum(); //digunakan untuk menghitung jumlah dari semua elemen
     }
 
-    static double multiply(double... operands) {
-        return DoubleStream.of(operands)
-                .reduce(1, (a, b) -> a * b);
+    static double multiply(double... operands) { // Baris ini mendefinisikan metode statis bernama multiply dengan tipe pengembalian double.
+        return DoubleStream.of(operands)//digunakan untuk mengubah larik (array) bilangan pecahan ganda (operands) menjadi sebuah aliran (stream) bilangan pecahan ganda.
+                .reduce(1, (a, b) -> a * b);//digunakan untuk menggabungkan elemen-elemen dalam aliran dengan suatu operasi yang ditentukan
     }
 }
--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -4,15 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class calculatorTest {
 
-    calculator cal = new calculator();
+    calculator cal = new calculator(); //Baris ini membuat objek dari kelas calculator yang akan diuji.
 
     @org.junit.jupiter.api.Test
     @DisplayName("Add two numbers")
-    void add() {
-        assertEquals(4, cal.add(2, 2));
+    void add() { //Baris ini mendefinisikan metode pengujian add. Metode ini akan memeriksa hasil dari metode add dalam kelas calculator.
+        assertEquals(4, cal.add(2, 2));//Baris ini menggunakan metode assertEquals dari kelas Assertions
+        // untuk memeriksa apakah hasil dari cal.add(2, 2) sama dengan 4.
     }
 
     @org.junit.jupiter.api.Test
-    void multiply() {
+    void multiply() {//Baris ini mendefinisikan metode pengujian multiply. Metode ini akan digunakan untuk menguji metode multiply dalam kelas calculator.
     }
 }
